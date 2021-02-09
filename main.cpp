126dbd71304abcd428f628c179679b92efc73753
biancareusch <reusch.bianca@gmail.com>
2021-02-09 13:08:14 -0600

    added labs 2 and 3

--- main.cpp
+++ main.cpp
@@ -2,10 +2,21 @@
 // cin is used to read character data into a string object.
 #include <iostream>
 #include <string>
+#include <iomanip>
+void Lab1();
+void Lab2();
+void Lab3();
+
 using namespace std;
 
 int main()
 {
+    Lab1();
+    Lab2();
+    Lab3();
+}
+
+void Lab1(){
     string name;
     string last;
     string city;
@@ -33,7 +44,26 @@ int main()
     cout << "Hello, " << name << endl;
     cout << "You live in " << city << endl;
 
+}
 
+void Lab2(){
+        int r;
+        double pi = 3.14;
+        cin >> r;
 
-    return 0;
+        double area = pi * r * r;
+        cout << setprecision(2) << fixed << area;
+}
+
+void Lab3(){
+    char ch;
+
+    cout << "This program has paused. Press Enter to continue.\n";
+    cin.get(ch);
+    cout << "It has paused a second time. Please press Enter again.\n";
+    ch = cin.get();
+    cout << "It has paused a third time. Please press Enter again.\n";
+    cin.get();
+    cout << "Thank you!";
+
 }
